e9e12d5d1f0895a3bf5a098b67a69dd5dc86fbfa
Adebowale Adedayo <adeniceadebo@gmail.com>
2025-10-12 21:53:06 +0100

    changes

--- main.tf
+++ main.tf
@@ -8,23 +8,43 @@ terraform {
     }
   }
 
+  # Terraform backend for remote state storage
   backend "s3" {
-    bucket = "my-zoomcamp-bucket-2025"
+    bucket = "my-zoomcamp-bucket-2025-adebowale"  
     key    = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
+# Configure AWS provider
 provider "aws" {
   region = var.aws_region
 }
 
+# Import your local SSH public key into AWS
+resource "aws_key_pair" "zoomcamp_key" {
+  key_name   = "zoomcamp-key"
+  public_key = file("~/.ssh/zoomcamp-key.pub")
+}
+
+# Launch a small EC2 instance for testing
+resource "aws_instance" "zoomcamp_ec2" {
+  ami           = "ami-091d7d61336a4c68f"  # Amazon Linux 2 (us-east-1)
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.zoomcamp_key.key_name
+
+  tags = {
+    Name = "zoomcamp-ec2-demo"
+  }
+}
+
 # Create a demo S3 bucket
 resource "aws_s3_bucket" "demo" {
-  bucket        = var.s3_bucket_name
+  bucket        = "my-zoomcamp-bucket-2025-adebowale-demo"
   force_destroy = true
 }
 
+# Add lifecycle policy: move objects to Infrequent Access after 30 days
 resource "aws_s3_bucket_lifecycle_configuration" "demo_lifecycle" {
   bucket = aws_s3_bucket.demo.id
 
@@ -48,6 +68,10 @@ output "bucket_arn" {
   value = aws_s3_bucket.demo.arn
 }
 
-output "bucket_region" {
-  value = aws_s3_bucket.demo.region
+output "ec2_instance_id" {
+  value = aws_instance.zoomcamp_ec2.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.zoomcamp_ec2.public_ip
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,42 @@
-variable "bq_data_set_name" {
-  description = "BigQuery Dataset Name"
-  type        = string
-  default     = "demo"
-}
-
+# AWS region for all resources
 variable "aws_region" {
-  description = "AWS Region to create resources in"
+  description = "AWS Region where resources will be deployed"
   type        = string
   default     = "us-east-1"
 }
 
-variable "s3_bucket_name" {
-  description = "S3 Bucket Name"
+# Main S3 bucket name for Terraform backend
+variable "s3_backend_bucket" {
+  description = "Existing S3 bucket used for Terraform state storage (backend)"
   type        = string
-  default     = "my-zoomcamp-bucket-2025"
+  default     = "my-zoomcamp-bucket-2025-adebowale"
 }
 
+# Demo S3 bucket name for project data
+variable "s3_bucket_name" {
+  description = "Name of the demo S3 bucket created by Terraform"
+  type        = string
+  default     = "my-zoomcamp-bucket-2025-adebowale-demo"
+}
+
+# AWS S3 storage class for lifecycle transitions
 variable "aws_storage_class" {
-  description = "AWS Storage Class for lifecycle management"
+  description = "Storage class for objects transitioned by lifecycle policy"
   type        = string
   default     = "STANDARD_IA"
 }
+
+# EC2 instance type
+variable "ec2_instance_type" {
+  description = "The type of EC2 instance to launch"
+  type        = string
+  default     = "t2.micro"
+}
+
+# Key pair name (used to SSH into EC2)
+variable "ec2_key_name" {
+  description = "The name of the key pair to use for EC2 access"
+  type        = string
+  default     = "zoomcamp-key"
+}
+
